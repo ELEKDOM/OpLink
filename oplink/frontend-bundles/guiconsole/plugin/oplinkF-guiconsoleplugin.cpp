892ac4066ce315d2273b428d16145c50c10390b1
Christophe Mars <c.mars@elekdom.fr>
2025-12-24 11:37:30 +0100

    OpLink Qt6 : backend & frontend cmake build and runtime install, initial working version.

---
+++ oplink/frontend-bundles/guiconsole/plugin/oplinkF-guiconsoleplugin.cpp
@@ -0,0 +1,37 @@
+// Copyright (C) 2025 ELEKDOM Christophe Mars c.mars@elekdom.fr
+// 
+// This file is part of OpLink.
+// 
+// OpLink is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// OpLink is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with PlugFrame. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "oplinkF-guiconsoleplugin.h"
+#include "olfconsole.h"
+
+OlfConsolePlugin::OlfConsolePlugin()
+{
+
+}
+
+OlfConsolePlugin::~OlfConsolePlugin()
+{
+
+}
+
+plugframe::Bundle4PluginInterface *OlfConsolePlugin::createImplementation()
+{
+    return new OlfConsole;
+}
+
+PF_bindServicesImplementations_DEF(OlfConsolePlugin)
